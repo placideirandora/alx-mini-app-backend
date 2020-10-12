275b34aea00357ef213f06476f1f1878d63fce36
Placide IRANDORA <placideirandora@gmail.com>
2020-10-12 15:07:03 +0200

    Make a Schema for Validating User Profile Info

--- src/helpers/schemas.js
+++ src/helpers/schemas.js
@@ -17,3 +17,9 @@ export const changePasswordSchema = Joi.object().keys({
   oldPassword: Joi.string().trim().required(),
   newPassword: Joi.string().trim().required(),
 });
+
+export const updateProfileSchema = Joi.object().keys({
+  firstName: Joi.string().max(30).trim().required(),
+  lastName: Joi.string().max(30).trim().required(),
+  userName: Joi.string().max(30).trim().required(),
+});
--- src/middleware/schemaValidator.js
+++ src/middleware/schemaValidator.js
@@ -2,6 +2,7 @@ import {
   signUpSchema,
   signInSchema,
   changePasswordSchema,
+  updateProfileSchema,
 } from '../helpers/schemas';
 
 export const signUpSchemaValidator = (req, res, next) => {
@@ -34,6 +35,20 @@ export const signInSchemaValidator = (req, res, next) => {
 
 export const changePasswordSchemaValidator = (req, res, next) => {
   const { error } = changePasswordSchema.validate(req.body);
+
+  if (error) {
+    const message = error.details[0].message.replace(/\\|(")/g, '');
+
+    return res.status(400).json({
+      error: message,
+    });
+  }
+
+  next();
+};
+
+export const updateProfileSchemaValidator = (req, res, next) => {
+  const { error } = updateProfileSchema.validate(req.body);
 
   if (error) {
     const message = error.details[0].message.replace(/\\|(")/g, '');
