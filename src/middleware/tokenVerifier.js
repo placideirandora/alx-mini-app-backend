74050814672422fc0f2e1857e5389bb4db6fc38a
Placide IRANDORA <placideirandora@gmail.com>
2020-10-12 14:31:04 +0200

    Create Middleware for Authenticating Users and Protecting the Endpoint Routes

---
+++ src/middleware/tokenVerifier.js
@@ -0,0 +1,44 @@
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+
+import { User } from '../database/models/User';
+
+config();
+
+export class TokenVerification {
+  /**
+   * This method verifies a token.
+   * @param {object} req Request
+   * @param {object} res Response
+   * @param {object} next Pass to next method.
+   * @returns {object} Error message
+   */
+  static verifyToken(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      res.status(403).json({ error: 'Please log in or Register' });
+    } else {
+      jwt.verify(token, process.env.SECRET_KEY, async (error, decoded) => {
+        if (error) {
+          return res.status(403).json({ error: `${error.message}` });
+        }
+
+        const registeredUser = await User.findOne({
+          where: { email: decoded.email },
+        });
+
+        if (registeredUser) {
+          req.user = registeredUser;
+
+          return next();
+        }
+
+        return res.status(403).json({
+          status: 403,
+          error: 'User not recognized. Please register and try again.',
+        });
+      });
+    }
+  }
+}
