60c8527f0efd0f5f5cf0596659e57c9a50b5795c
Placide IRANDORA <placideirandora@gmail.com>
2020-10-12 07:10:36 +0200

    Change the Approach for fixing the Model Access error in the Controller

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,51 +1,38 @@
-'use strict';
-const { Model } = require('sequelize');
+import { DataTypes } from 'sequelize';
+import { sequelize } from './index';
 
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init(
-    {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-      },
-      firstName: {
-        allowNull: false,
-        type: DataTypes.STRING,
-      },
-      lastName: {
-        allowNull: false,
-        type: DataTypes.STRING,
-      },
-      userName: {
-        type: DataTypes.STRING,
-        unique: false,
-      },
-      email: {
-        allowNull: false,
-        type: DataTypes.STRING,
-        unique: false,
-      },
-      password: {
-        allowNull: false,
-        type: DataTypes.STRING,
-      },
+export const User = sequelize.define(
+  'User',
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
     },
-    {
-      sequelize,
-      modelName: 'User',
-    }
-  );
-  return User;
-};
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
