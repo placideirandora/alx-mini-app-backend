154527fac1f7279afaf086d97a5d5a4abe58122d
Placide IRANDORA <placideirandora@gmail.com>
2020-10-12 08:40:34 +0200

    Add a Schema for Sign In Validation

--- src/helpers/schemas.js
+++ src/helpers/schemas.js
@@ -1,9 +1,14 @@
 import Joi from 'joi';
 
-export const userSchema = Joi.object().keys({
+export const signUpSchema = Joi.object().keys({
   firstName: Joi.string().max(30).required(),
   lastName: Joi.string().max(30).required(),
   userName: Joi.string().max(30).required(),
   email: Joi.string().email({ minDomainSegments: 2 }).required(),
   password: Joi.string().max(30).required(),
+});
+
+export const signInSchema = Joi.object().keys({
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  password: Joi.string().required(),
 });
--- src/middleware/schemaValidator.js
+++ src/middleware/schemaValidator.js
@@ -1,7 +1,21 @@
-import { userSchema } from '../helpers/schemas';
+import { signUpSchema, signInSchema } from '../helpers/schemas';
 
-export const userSchemaValidator = (req, res, next) => {
-  const { error } = userSchema.validate(req.body);
+export const signUpSchemaValidator = (req, res, next) => {
+  const { error } = signUpSchema.validate(req.body);
+
+  if (error) {
+    const message = error.details[0].message.replace(/\\|(")/g, '');
+
+    return res.status(400).json({
+      error: message,
+    });
+  }
+
+  next();
+};
+
+export const signInSchemaValidator = (req, res, next) => {
+  const { error } = signInSchema.validate(req.body);
 
   if (error) {
     const message = error.details[0].message.replace(/\\|(")/g, '');
